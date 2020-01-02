1026f8448d5400ace21f9625104c898fe2f15122
Luis F. Bilecki <luis.bilecki@gmail.com>
2020-01-02 19:38:43 -0300

    Configure endpoints with controller

--- src/server.js
+++ src/server.js
@@ -3,15 +3,29 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const redisInstance = require('./services/redis');
+
+const cartController = require('./controllers/cartController');
 
 const app = express();
 
 app.use(morgan('combined'));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.send('testing'); 
-});
+// Get cart
+app.get('/carts/:session', (req, res) => cartController.getCompleteCart(req, res, redisInstance));
+
+// Add item to cart
+app.post('/carts/:session/items', (req, res) => cartController.addCartItem(req, res, redisInstance));
+
+// Update cart item
+app.put('/carts/:session/items/:item', (req, res) => cartController.updateCartItem(req, res, redisInstance));
+
+// Delete cart item
+app.delete('/carts/:session/items', (req, res) => cartController.deleteCartItem(req, res, redisInstance));
+
+// Delete user cart
+app.delete('/carts/:session', (req, res) => cartController.clearCart(req, res, redisInstance));
 
 const serverPort = process.env.SERVER_PORT || 3000;
 
