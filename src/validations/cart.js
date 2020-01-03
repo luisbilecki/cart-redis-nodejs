aa70e1c3d03cf30d0d6c188b1d346f96f22e4e3c
Luis F. Bilecki <luis.bilecki@gmail.com>
2020-01-03 14:18:40 -0300

    Fix in validation rules for cart

--- src/validations/cart.js
+++ src/validations/cart.js
@@ -1,7 +1,7 @@
 const { body, param } = require('express-validator');
 
 module.exports = {
-  addItemRules: [body('quantity').isInt({ gt: '1'}), body('sku').isInt()],
-  updateItemRules: [param('sku').isInt(), body('quantity').isInt({ gt: '1'})],
+  addItemRules: [body('quantity').isInt({ gt: 0 }), body('sku').isInt()],
+  updateItemRules: [param('sku').isInt(), body('quantity').isInt({ gt: 0 })],
   deleteItemRules: [param('sku').isInt()],
 };
