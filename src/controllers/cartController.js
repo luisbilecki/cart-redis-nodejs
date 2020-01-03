82d2787e78c325512a3f5267c5e8c0854065da18
Luis F. Bilecki <luis.bilecki@gmail.com>
2020-01-03 11:34:40 -0300

    Implement cart controller methods

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -1,25 +1,60 @@
-const getCompleteCart = (req, res, redis) => {
-  return res.json('ok');
+const DefaultError = require('../errors/defaultError');
+
+const cartKey = (session) => `cart:${session}`;
+const expireCartTime = process.env.EXPIRE_CART_TIME || 1800; // in seconds
+
+const getCart = async(req, res, redis) => {
+  const { session } = req.params;
+
+  const items = redis.hgetall(cartKey(session));
+  res.json({ success: true, items });
 };
 
-const addCartItem = (req, res, redis) => {
-  return res.json('ok');
+const addCartItem = async(req, res, redis) => {
+  const { session } = req.params;
+  const { sku, quantity } = req.body;
+  const key = cartKey(session);
+
+  const result = await redis.hset(key, sku, quantity);
+
+  // It was better that expire command runs with hset in only one command
+  await redis.expire(key, expireCartTime);
+
+  res.json({ success: !!result });
 };
 
-const updateCartItem = (req, res, redis) => {
-  return res.json('ok');
+const updateCartItem = async(req, res, next, redis) => {
+  const { session, item } = req.params;
+  const { quantity } = req.body;
+  const key = cartKey(session);
+
+  const cartItem = await redis.hget(key, item);
+
+  // Item found! Can update!
+  if (cartItem) {
+    const result = await redis.hmset(cartKey(session), item, quantity);
+    res.json({ success: !!result });
+  } else {
+    next(DefaultError.notFound(req, 'Item not found'));
+  }
 };
 
-const deleteCartItem = (req, res, redis) => {
-  return res.json('ok');
+const deleteCartItem = async(req, res, redis) => {
+  const { session, item } = req.params;
+  const result = await redis.hdel(cartKey(session), item);
+
+  res.json({ success: !!result });
 };
 
-const clearCart = (req, res, redis) => {
-  return res.json('ok');
+const clearCart = async(req, res, redis) => {
+  const { session } = req.params;
+  const result = await redis.del(cartKey(session));
+  
+  res.json({ success: !!result });
 };
 
 module.exports = {
-  getCompleteCart,
+  getCart,
   addCartItem,
   updateCartItem,
   deleteCartItem,
--- src/server.js
+++ src/server.js
@@ -10,19 +10,21 @@ const cartController = require('./controllers/cartController');
 const app = express();
 
 app.use(morgan('combined'));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// TODO: VALIDATE DATA
 // Get cart
-app.get('/carts/:session', (req, res) => cartController.getCompleteCart(req, res, redisInstance));
+app.get('/carts/:session', (req, res) => cartController.getCart(req, res, redisInstance));
 
 // Add item to cart
 app.post('/carts/:session/items', (req, res) => cartController.addCartItem(req, res, redisInstance));
 
 // Update cart item
-app.put('/carts/:session/items/:item', (req, res) => cartController.updateCartItem(req, res, redisInstance));
+app.put('/carts/:session/items/:item', (req, res, next) => cartController.updateCartItem(req, res, next, redisInstance));
 
-// Delete cart item
-app.delete('/carts/:session/items', (req, res) => cartController.deleteCartItem(req, res, redisInstance));
+// Delete cart items
+app.delete('/carts/:session/items/:item', (req, res) => cartController.deleteCartItem(req, res, redisInstance));
 
 // Delete user cart
 app.delete('/carts/:session', (req, res) => cartController.clearCart(req, res, redisInstance));
